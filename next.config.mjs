9e5ecc06d010e483f974130176aa1145f9f87bf7
Flogert <flogert@rocketmail.com>
2025-08-22 16:00:00 -0400

    Configure for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  basePath: '/anagrams',
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
